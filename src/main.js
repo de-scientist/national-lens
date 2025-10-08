fdefb84674e136c1ea5bd5f0b10ca6dff8e828c0
de-scientist <gitaumark502@gmail.com>
2025-10-08 15:20:49 +0300

    add DOM elements

--- src/main.js
+++ src/main.js
@@ -1,24 +1,16 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+//add DOM elements
+const input = document.getElementById("form-name-input");
+const btn = document.querySelector(".search-btn");
+const result = document.querySelector(".result");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//add an event listener
+btn.addEventListener("click", async (ev) => {
+  ev.preventDefault()
+  const providedName  = input.value;
 
-setupCounter(document.querySelector('#counter'))
+  if (providedName === "") {
+    result.textContent = `Please provide a name`;
+    return
+  }
+
+})
